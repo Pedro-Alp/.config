60efc7e11dd02c6c030149b0b5336d4e0d9fc56e
Pedro LP. <102270133+Pedro-Alp@users.noreply.github.com>
2025-09-27 00:28:49 +0200

    Update lsp.lua

--- nvim/lua/config/plugins/lsp.lua
+++ nvim/lua/config/plugins/lsp.lua
@@ -1,30 +1,54 @@
 return {
   {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      'saghen/blink.cmp',
-      {
-        "folke/lazydev.nvim",
-        ft = "lua", -- only load on lua files
-        opts = {
-          library = {
-            -- See the configuration section for more details
-            -- Load luvit types when the `vim.uv` word is found
-            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-          },
-        },
-      },
-    },
+    "hrsh7th/nvim-cmp",
+    lazy = true,
+    dependencies = { "hrsh7th/cmp-nvim-lsp" },
+  },
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
     config = function()
-      local capabilities = require('blink.cmp').get_lsp_capabilities()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    dependencies = { "hrsh7th/cmp-nvim-lsp" },
+    config = function()
+      local cmp_nvim_lsp = require("cmp_nvim_lsp")
+      local capabilities = vim.tbl_deep_extend(
+        "force",
+        {},
+        vim.lsp.protocol.make_client_capabilities(),
+        cmp_nvim_lsp.default_capabilities()
+      )
+
       vim.lsp.config('*', {
         capabilities = capabilities
       })
       vim.lsp.config.lua_ls = {
         filetypes = {'lua'},
       }
+      vim.lsp.config.jdtls = {
+        filetypes = {'java'},
+      }
       vim.lsp.enable('lua_ls')
+      vim.lsp.enable('jdtls')
 
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+      vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
 
       vim.api.nvim_create_autocmd('LspAttach', {
         callback = function(args)
@@ -43,5 +67,5 @@ return {
         end,
       })
     end,
-  }
+  },
 }
